3ac86f33ddc71f11382381de7c5cae28a88a7f28
christopher sousa <christopher.sousa@stage.consulting>
2024-05-09 01:04:13 -0300

    feat: configure tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          100: "var(--secondary-100)",
+        }
+      },
+    },
   },
   plugins: [],
 } satisfies Config
