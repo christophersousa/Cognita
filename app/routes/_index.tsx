ab7f778ca89d2c79958bdc4ebd9cd7354bfdf468
christopher sousa <christopher.sousa@stage.consulting>
2024-05-09 20:30:50 -0300

    feat: loading data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,6 +7,7 @@ import Modal from "~/components/Modal/ModalStep";
 import neo4j from 'neo4j-driver';
 import { fetchDataTrail } from "~/service/neo4js";
 import { IListStepsByTrail } from "~/interface/interfaces";
+import { Loading } from "~/components/Loading/Loading";
 
 export const meta: MetaFunction = () => {
   return [
@@ -18,10 +19,13 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const [popUp, setPopUp] = useState<boolean>(false)
   const [data, setData] = useState<IListStepsByTrail>();
+  const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     const request = async ()=>{
+      setLoading(true)
       const data = await fetchDataTrail()
+      setLoading(false)
       setData(data)
     }
     request()
@@ -29,25 +33,30 @@ export default function Index() {
   return (
     <main className="flex justify-center py-20">
       <Container>
-        {/* Header */}
-        <div className="flex justify-between items-center">
-          <h1 className="text-title font-semibold text-secondary-100">{data?.trail.title}</h1>
-          <div 
-            className="flex items-center py-3 px-4 gap-1 bg-primary text-base text-white font-semibold cursor-pointer rounded-xl hover:bg-primary-100"
-            onClick={()=>setPopUp(true)}
-            >
-            <img
-              src={AddIcon}
-              className="w-5 h-5"
-            />
-            Adicionar passos
+        {/* loadding */}
+        <Loading loading={loading}/>
+        {/* content */}
+        <div style={{display: loading?"none": "block"}} >
+          {/* Header */}
+          <div className="flex justify-between items-center">
+            <h1 className="text-title font-semibold text-secondary-100">{data?.trail.title}</h1>
+            <div 
+              className="flex items-center py-3 px-4 gap-1 bg-primary text-base text-white font-semibold cursor-pointer rounded-xl hover:bg-primary-100"
+              onClick={()=>setPopUp(true)}
+              >
+              <img
+                src={AddIcon}
+                className="w-5 h-5"
+              />
+              Adicionar passos
+            </div>
           </div>
-        </div>
-        {/* List of steps */}
-        <div className="flex flex-col gap-6 mt-10">
-          {data?.steps.map(step => (
-            <CardStep key={step.id} title={step.title} content={step.content} id={step.id}/>
-          ))}
+          {/* List of steps */}
+          <div className="flex flex-col gap-6 mt-10">
+            {data?.steps.map(step => (
+              <CardStep key={step.id} title={step.title} content={step.content} id={step.id}/>
+            ))}
+          </div>
         </div>
         {/* Modal */}
         {popUp && <Modal setPopUp={setPopUp} />}
