3bd87c944a9679ba45863c3da78777380844442a
christopher sousa <christopher.sousa@stage.consulting>
2024-05-10 09:46:32 -0300

    feat: add button to return to home

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -12,8 +12,9 @@ function page404(){
             <Container>
                 <div className="text-center absolute top-1/2 -translate-y-1/2">
                     <h1 className="text-7xl tracking-tight font-extrabold text-primary mb-4">404</h1>
-                    <h2 className="text-3xl tracking-tight font-bold mb-4">Ah, parece que você solicitou uma Trilha desconhecido!</h2>
-                    <p>Desculpe-nos, não conseguimos exibir as informações solicitadas neste momento. Por favor, tente novamente mais tarde.</p>
+                    <h2 className="text-3xl tracking-tight font-bold mb-4">Ah, parece que você solicitou uma Trilha desconhecida!</h2>
+                    <p className="mb-6">Desculpe-nos, não conseguimos exibir as informações solicitadas neste momento. Por favor, tente novamente mais tarde.</p>
+                    <a href="/" className="py-3 px-4 bg-primary font-semibold text-white rounded-lg">Voltar para Home!</a>
                 </div>
             </Container>
         </section>
